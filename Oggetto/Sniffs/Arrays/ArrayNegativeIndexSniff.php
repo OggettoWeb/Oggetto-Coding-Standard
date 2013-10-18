be80f515d14cce57960a0b35ebbd33ecf386ea9e
denis belov <deniswhite8@gmail.com>
2013-10-18 19:45:40 +0400

    fix 15 issue

--- Oggetto/Sniffs/Arrays/ArrayNegativeIndexSniff.php
+++ Oggetto/Sniffs/Arrays/ArrayNegativeIndexSniff.php
@@ -8,7 +8,7 @@ class Oggetto_Sniffs_Arrays_ArrayNegativeIndexSniff implements PHP_CodeSniffer_S
      */
     public function register()
     {
-        return array(T_VARIABLE, T_ARRAY);
+        return array(T_VARIABLE, T_DOUBLE_ARROW);
     }
 
 
@@ -24,19 +24,32 @@ class Oggetto_Sniffs_Arrays_ArrayNegativeIndexSniff implements PHP_CodeSniffer_S
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         $tokens = $phpcsFile->getTokens();
+        $isError = false;
 
-        // In the case of negative index, after the variable goes a parenthesis, after unary minus and integer.
-        // Or, after the array keyword goes a bracket, after unary minus and integer.
+        // In the case of negative index, after the variable goes a bracket '[', after unary minus and integer.
+        // Or, before double arrow '=>' stands unary minus and integer.
         // Possibly, tokens are separated by whitespaces.
-        $openBracket = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
-        $minus = $phpcsFile->findNext(T_WHITESPACE, ($openBracket + 1), null, true);
-        $index = $phpcsFile->findNext(T_WHITESPACE, ($minus + 1), null, true);
 
-        if (($tokens[$stackPtr]['code'] === T_VARIABLE && $tokens[$openBracket]['code'] === T_OPEN_SQUARE_BRACKET &&
-            $tokens[$minus]['code'] === T_MINUS && $tokens[$index]['code'] === T_LNUMBER) ||
-            ($tokens[$stackPtr]['code'] === T_ARRAY && $tokens[$openBracket]['code'] === T_OPEN_PARENTHESIS &&
-            $tokens[$minus]['code'] === T_MINUS && $tokens[$index]['code'] === T_LNUMBER)) {
+        if ($tokens[$stackPtr]['code'] === T_VARIABLE) {
+            $openBracket = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
+            $minus = $phpcsFile->findNext(T_WHITESPACE, ($openBracket + 1), null, true);
+            $index = $phpcsFile->findNext(T_WHITESPACE, ($minus + 1), null, true);
 
+            if ($tokens[$openBracket]['code'] === T_OPEN_SQUARE_BRACKET &&
+                $tokens[$minus]['code'] === T_MINUS && $tokens[$index]['code'] === T_LNUMBER) {
+                $isError = true;
+            }
+        } else { // T_DOUBLE_ARROW
+            $index = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
+            $minus = $phpcsFile->findPrevious(T_WHITESPACE, ($index - 1), null, true);
+
+            if ($tokens[$minus]['code'] === T_MINUS && $tokens[$index]['code'] === T_LNUMBER) {
+                $isError = true;
+            }
+        }
+       
+
+        if ($isError) {
             $message = "Array index can not be negative";
             $phpcsFile->addError($message, $stackPtr, 'Found');
         }
